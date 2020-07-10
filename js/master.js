3c7559f295b36dedf5afa8ea8385fec298b54521
guinhx <123DJcaptain>
2020-07-10 18:16:42 -0300

    finalized

--- js/master.js
+++ js/master.js
@@ -1,6 +1,7 @@
 function handlePage() {
     updateActiveItem();
     registerStoreListener();
+    registerFormListener();
 }
 
 function updateActiveItem() {
@@ -57,32 +58,36 @@ function registerStoreListener() {
 
 function registerFormListener() {
     var numberInput = document.getElementById('inputPhone');
-    numberInput.addEventListener('keyup', function (event) {
-        var number = event.target.value.replace(/[^\d]/g, "").replace(/\D/g,'');
-        event.target.value = number;
-        if (number.length == 11) {
-            var phone = "(" + number.substr(0, 2) + ") " + number.substr(2, 5) + "-" + number.substr(7);
-            event.target.value = phone;
-        }
-    });
-    var mailInput = document.getElementById('inputMail');
-    mailInput.addEventListener('keyup', function(event) {
-        var text = event.target.value;
-        var sliced = text.split(".");
-        if(!text.includes("@") || sliced.length < 2) {
-            if(event.target.classList.contains("success")) {
-                event.target.classList.remove("success");
+    if(numberInput) {
+        numberInput.addEventListener('keyup', function (event) {
+            var number = event.target.value.replace(/[^\d]/g, "").replace(/\D/g,'');
+            event.target.value = number;
+            if (number.length == 11) {
+                var phone = "(" + number.substr(0, 2) + ") " + number.substr(2, 5) + "-" + number.substr(7);
+                event.target.value = phone;
             }
-        }else{
-            event.target.classList.add("success");
-        }
-    });
-    mailInput.addEventListener('focusout', function(event) {
-        var text = event.target.value;
-        var sliced = text.split(".");
-        if(!text.includes("@") || sliced.length < 2) {
-            alert("Invalid e-mail address.");
-            event.target.value = "";
-        }
-    });
+        });
+    }
+    var mailInput = document.getElementById('inputMail');
+    if(mailInput) {
+        mailInput.addEventListener('keyup', function(event) {
+            var text = event.target.value;
+            var sliced = text.split(".");
+            if(!text.includes("@") || sliced.length < 2) {
+                if(event.target.classList.contains("success")) {
+                    event.target.classList.remove("success");
+                }
+            }else{
+                event.target.classList.add("success");
+            }
+        });
+        mailInput.addEventListener('focusout', function(event) {
+            var text = event.target.value;
+            var sliced = text.split(".");
+            if(!text.includes("@") || sliced.length < 2) {
+                alert("Invalid e-mail address.");
+                event.target.value = "";
+            }
+        });
+    }
 }
